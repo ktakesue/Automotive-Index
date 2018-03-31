deda5b72f9a12b665ee56b659b61a94aeeeaa0b3
ktakesue <ktakesue@aol.com>
2018-03-31 13:27:57 -1000

    finished exercise

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -9,4 +9,10 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+
+CREATE INDEX get_make_codes ON car_models (make_code);
+
+CREATE INDEX get_model_codes ON car_models (model_code);
+
+CREATE INDEX get_years ON car_models (year);
